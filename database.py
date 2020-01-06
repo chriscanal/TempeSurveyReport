6ea2c30d3728b635342626307bd0ee72c9cec1a7
Chris Canal on Macbook Air <christophercanal4@gmail.com>
2020-01-06 15:54:01 -0600

    added joint tables

--- database.py
+++ database.py
@@ -7,6 +7,34 @@ db = create_engine("sqlite:///:memory:", echo=False)
 base = declarative_base()
 
 
+class QuestionSurveyJoint(base):
+    __tablename__ = "question_surveys"
+    survey_id = Column(Integer, ForeignKey("surveys.survey_id"), primary_key=True)
+    question_id = Column(Integer, ForeignKey("questions.question_id"), primary_key=True)
+    section_id = Column(Integer, ForeignKey("sections.section_id"), primary_key=True)
+
+
+class SectionPossibleResponseJoint(base):
+    __tablename__ = "section_possible_response"
+    survey_id = Column(Integer, ForeignKey("surveys.survey_id"), primary_key=True)
+    question_id = Column(
+        Integer, ForeignKey("possible_responses.possible_response_id"), primary_key=True
+    )
+
+
+class AnswersJoint(base):
+    __tablename__ = "answers"
+    survey_id = Column(Integer, ForeignKey("surveys.survey_id"), primary_key=True)
+    responder_id = Column(
+        Integer, ForeignKey("responder_id.responder_id"), primary_key=True
+    )
+    question_id = Column(Integer, ForeignKey("questions.question_id"), primary_key=True)
+    section_id = Column(Integer, ForeignKey("sections.section_id"), primary_key=True)
+    possible_response_id = Column(
+        Integer, ForeignKey("possible_responses.possible_response_id"), primary_key=True
+    )
+
+
 class Survey(base):
     __tablename__ = "surveys"
     survey_id = Column(Integer, primary_key=True)

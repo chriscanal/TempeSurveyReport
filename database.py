3da1bf2b85729c2ecd1c6546e9d3511f39254202
Chris Canal on Macbook Air <christophercanal4@gmail.com>
2020-01-06 14:20:21 -0600

    created outline of database

---
+++ database.py
@@ -0,0 +1,70 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+db = create_engine("sqlite:///:memory:", echo=False)
+base = declarative_base()
+
+
+class Survey(base):
+    __tablename__ = "surveys"
+    survey_id = Column(Integer, primary_key=True)
+    survey_name = Column(String)
+    company = Column(String)
+    year = Column(Integer)
+
+
+class Question(base):
+    __tablename__ = "questions"
+    question_id = Column(Integer, primary_key=True)
+    question_survey_number = Column(String)
+    question_text = Column(String)
+
+
+class Section(base):
+    __tablename__ = "sections"
+    section_id = Column(Integer, primary_key=True)
+    section_name = Column(String)
+
+
+class PossibleResponse(base):
+    __tablename__ = "possible_responses"
+    possible_response_id = Column(Integer, primary_key=True)
+    possible_response_text = Column(String)
+    possible_response_number = Column(Integer)
+
+
+class Responder(base):
+    __tablename__ = "responders"
+    responder_id = Column(Integer, primary_key=True)
+
+
+if __name__ == "__main__":
+    Session = sessionmaker(db)
+    session = Session()
+
+    base.metadata.create_all(db)
+
+    # Create
+    tempe_survey = Survey(
+        survey_id=1,
+        survey_name="Tempe Employee Satisfaction Survey",
+        company="City of Tempe",
+        year=2016,
+    )
+    session.add(tempe_survey)
+    session.commit()
+
+    # Read
+    surveys = session.query(Survey)
+    for s in surveys:
+        print(s.survey_name)
+
+    # Update
+    tempe_survey.survey_name = "Tempe,AZ Employee Satisfaction Survey"
+    session.commit()
+
+    # Delete
+    session.delete(tempe_survey)
+    session.commit()
